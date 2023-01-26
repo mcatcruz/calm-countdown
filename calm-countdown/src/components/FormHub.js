44dd7b10b384d146b11144ca6f1949aaa014e706
Monica Cruz <mcatcruz@gmail.com>
2023-01-26 13:25:01 -0800

    Worked on seeForm API route. Started work on FormHub component.

---
+++ calm-countdown/src/components/FormHub.js
@@ -0,0 +1,11 @@
+import React from "react";
+import propTypes from "prop-types";
+import styles from "../styles/Home.module.css";
+import { Fade } from "react-awesome-reveal";
+
+// FormHub holds the initial state of all the forms through useState.
+const FormHub = () => {
+    const[formStep, setFormStep] = React.useState(0);
+    const [] = React.useState("")
+    const [] = React.useState("");
+}
---
+++ calm-countdown/src/pages/api/seeForm.js
@@ -0,0 +1,25 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+// Note: This endpoint is server-side only.
+// TO-DO: Attach this API to a database.
+// TO-DO: What info will be in req.body? Figure out what we are checking for.
+
+export default function handler(req, res) {
+
+  // Get data submitted in req body
+  const body = req.body;
+
+  // Log response to console
+  console.log('body: ', body);
+
+  // Error handling - check for [WHAT INFO TO CHECK FOR?],
+  // and returns early if not found
+  if (!body.INFO) {
+    res.status(400).json({ data: '[INFO] was not found.' })
+  }
+  
+  // Everything goes well
+  res.status(200).json({ data: `${body.INFO}`})
+}
+
+
