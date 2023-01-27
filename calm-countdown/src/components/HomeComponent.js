7a77490bd645309d9a87527f5c60b95405a29d1a
Monica Cruz <mcatcruz@gmail.com>
2023-01-27 15:09:28 -0800

    Editing Homepage CSS.

--- calm-countdown/src/components/HomeComponent.js
+++ calm-countdown/src/components/HomeComponent.js
@@ -1,9 +1,15 @@
+import React from 'react';
+import styles from '../styles/HomeComponent.module.css'
+
 // This is the Home Page UI. 
 
 function HomeComponent(){
     return (
-        <>
-            <h1>Hello World</h1>
+        <>  
+            <div className={styles.container}>
+                <h1 className={styles.title}>Meet Calm</h1>
+                <h3 className={styles.subtitle}>Engage your senses. Return to the present.</h3>
+            </div>
         </>
     )
 }
