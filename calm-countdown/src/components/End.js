1d373a164d83cd831dd8881b3f788f1c23cec5c9
Monica Cruz <mcatcruz@gmail.com>
2023-02-14 18:10:56 -0800

    Added TODO memo to End component

--- calm-countdown/src/components/End.js
+++ calm-countdown/src/components/End.js
@@ -3,7 +3,9 @@ import styles from '../styles/HomeComponent.module.css'
 import { useState } from 'react'; 
 import HomeComponent from './HomeComponent';
 
-// This is the Home Page UI. 
+
+// 2/14  TODO: RESTART BUTTON GOES TO HOMECOMPONENT (SEE FORMHUB COMPONENT)
+
 
 function EndComponent(){
 
